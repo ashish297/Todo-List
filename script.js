96d893c6312ffd127928c18c3f45689e7a24f152
ashish297 <83005187+ashish297@users.noreply.github.com>
2023-07-21 11:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let taskArray = [];
+let taskArray = JSON.parse(localStorage.getItem('taskArray'))||[];
 let inputTaskElememt = document.getElementById('js-input-task');
 let inputDateElement = document.getElementById('js-input-date');
 let addTaskElement = document.querySelector('.js-add-task-button');
 let deleteTaskElement = document.querySelector('.js-delete-task-button');
+let str = JSON.parse(localStorage.getItem('str'))|| '';
+document.querySelector('.js-container1').innerHTML = str;
+
 
 addTaskElement.addEventListener('click', addEvent);
 
@@ -18,19 +21,21 @@ function addEvent() {
 
 function displayList () {
   
-  let str = '';
+  console.log(taskArray);
+  str = '';
   if(taskArray.length === 0){
     str = '';
   } else {
     taskArray.forEach((element,i)=> {
       str = str + `
       <div class="content">
-        <p class = "para">${element.task}</p>
+        <p class = "para task-title">${element.task}</p>
         <div class = "para-button">
           <p class = "para">${element.date}</p>
           <div class = "edit-remove-butto">
             <button class="button1" onclick = "
               editTask(${i});
+              
             ">Edit</button>
             <button class="button1" onclick = "
               taskArray.splice(${i},1);
@@ -41,10 +46,14 @@ function displayList () {
       </div>`
     });
   }
+
+  localStorage.setItem('taskArray', JSON.stringify(taskArray));
   document.querySelector('.js-container1').innerHTML = str;
+  localStorage.setItem('str', JSON.stringify(str));
 }
 
 function editTask(index) {
+  addTaskElement.innerHTML = 'Edit';
   inputTaskElememt.value = `${taskArray[index].task}`;
   inputDateElement.value = `${taskArray[index].date}`;
 
@@ -52,6 +61,13 @@ function editTask(index) {
   taskArray[index].task = `${objectForEdit.task}`;
   taskArray[index].date = `${objectForEdit.date}`;
   taskArray.splice(index, 1);
+
   displayList();
+  
 }
 
+function toggleButton () {
+  if (addTaskElement.innerHTML === 'Edit') {
+    addTaskElement.innerHTML = 'Add';
+  }
+}
